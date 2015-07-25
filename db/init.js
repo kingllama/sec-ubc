18f3eee2ef3b7ee8e60b1877e13f725f5a888fb8
shakir <shakirali2244@yahoo.com>
2015-07-25 15:39:48 -0700

    scheme sorta added

--- db/init.js
+++ db/init.js
@@ -2,15 +2,33 @@ con = require('./localconfig.js');
 
 
 // DOCS http://knexjs.org/#Schema
-con.knex.schema.createTable('drone', function(table) {
-	table.string('ip_addr');
-	table.string('name');
+con.knex.schema.createTable('admins', function(table) {
+	table.string('username').unique();
+	table.string('hash');
 	})
 
-	.createTable('user', function(table) {
-	table.string('email').unique();
-	table.string('hash');
-	table.string('salt');
+	.createTable('blog_posts', function(table) {
+	table.date('date');
+	table.text('content','longtext');
+	table.string('title');
+	})
+
+	.createTable('events', function(table) {
+	table.date('date');
+	table.string('title');
+	})
+
+	.createTable('members', function(table) {
+	table.string('title');
+	table.text('discription');
+	})
+
+	.createTable('conference', function(table) {
+	table.text('content','longtext');
+	})
+
+	.createTable('galaries', function(table) {
+	table.binary('picture');
 	})
 	
 	.catch(function(e) {
